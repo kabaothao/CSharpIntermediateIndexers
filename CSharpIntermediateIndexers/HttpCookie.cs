fbd6f8a4c4e4c3c296304dc2656286cd85f97d41
Kabao Thao <KabaoKThao@gillettechildrens.com>
2022-02-28 16:26:12 -0600

    updated notes and code

---
+++ CSharpIntermediateIndexers/HttpCookie.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpIntermediateIndexers
+{
+    public class HttpCookie
+    {
+        private readonly Dictionary<string, string> _dictionary;
+        public DateTime Expiry { get; set; }
+
+        public HttpCookie()
+        {
+            _dictionary = new Dictionary<string, string>(); 
+        }
+
+        public string this[string key]
+        {
+            get { return _dictionary[key]; } //we rely the get to store the key
+            set { _dictionary[key] = value; } //we rely key to 
+        }
+
+    }
+}
+
+
+/*
+ If you're not familiar with a dictionary it's a data type that resize in system that collections that
+
+generic and it uses a hash table to store data hash table has a special mechanism that makes it extremely
+
+fast to look up an item by its key.
+
+ s*/
--- CSharpIntermediateIndexers/Program.cs
+++ CSharpIntermediateIndexers/Program.cs
@@ -3,13 +3,40 @@
 
 using System;
 
-namespace MyApp // Note: actual namespace depends on the project name.
+namespace CSharpIntermediateIndexers // Note: actual namespace depends on the project name.
 {
-    internal class Program
+    internal static class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var cookie = new HttpCookie();
+            cookie["name"] = "Kabao";
+            Console.WriteLine(cookie["name"]);
         }
     }
 }
+
+
+
+
+/*
+What is an indexer?
+
+A mixer is a way to access elements in a class that represents a list of values.
+
+
+
+Now how can we declare an indexer and indexer is nothing but a property.
+
+So we can declare an indexer exactly like how we declare a property.
+
+So we have the data type like string.
+
+Then in sort of an identifier we use that this keyword.
+
+And here is the index or notation.
+
+So brackets and inside the bracket we have the type of indexer.
+
+So in this case with the typical key I want to find items by the key which should be a string.
+ */
